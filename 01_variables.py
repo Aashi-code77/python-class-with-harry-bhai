d8197eb8011747bd72bb827b9d572417b4c79284
Aashi-code77 <pandaoutlier@gmail.com>
2025-03-25 21:03:58 +0530

    Add files via upload
    python

---
+++ 01_variables.py
@@ -0,0 +1,6 @@
+a =1
+b =2
+c = "three"
+d = ( a+b )
+print(d )
+print(c)
---
+++ basic.py
@@ -0,0 +1,12 @@
+# print("hello world")
+# Module ka two type -- 1st build in module (preinstalled in python)
+#  2nd external Module ( Need to install using pip)
+import pyjokes # external module ( )
+joke = pyjokes.get_joke()
+print(joke)
+"""
+ a python REPL ( Read Evaluate Print Loop) 
+ or calculater
+"""
+# alt se line select krke ctrl+/ use krne se 1se jada line pr # aa jata h and we also use triple ""/ ''
+
